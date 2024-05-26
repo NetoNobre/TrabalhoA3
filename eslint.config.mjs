4379f6c7afe1451e850a2a0acde6f26cbb46731c
Neto Nobre <igornobre2016@hotmail.com>
2024-05-25 21:42:09 -0300

    Outra tentativa

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,17 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import nextConfig from 'eslint-config-next';
 
 export default [
   pluginJs.configs.recommended,
   pluginReactConfig,
+  nextConfig,
   {
     languageOptions: {
       globals: globals.browser,
+      ecmaVersion: 12,
+      sourceType: "module",
     },
     rules: {
       'react/no-unescaped-entities': 'off',  // Desativa a regra 'react/no-unescaped-entities'
